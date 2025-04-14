33a1364187d63d6e5026c937c34a6c5a9c83bc0b
Guilherme Bueno Martins <gui.bue@uninove.edu.br>
2025-04-14 11:15:34 +0000

    Acessar produto

--- java/sauce-demo-web-auto/src/test/java/br/saucedemowebauto/steps/ProductsSteps.java
+++ java/sauce-demo-web-auto/src/test/java/br/saucedemowebauto/steps/ProductsSteps.java
@@ -1,17 +1,44 @@
 package br.saucedemowebauto.steps;
 
+import java.util.Random;
+
 import org.junit.Assert;
 
+import br.saucedemowebauto.dto.ProductDto;
 import br.saucedemowebauto.pages.ProductsPage;
+import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
 
 public class ProductsSteps {
 
     private final ProductsPage page = new ProductsPage();
 
+    @Given("usuario escolhe produto {string} na pagina Products")
+    public void usuarioEscolheProdutoNaPaginaProducts(String name) {
+        if (name.isEmpty()) {
+            int index = new Random().nextInt(page.getQuantityOfProducts());
+            Hooks.productDto = page.getProductByIndex(index);
+        } else {
+            Hooks.productDto = page.getProductByName(name);
+        }
+    }
+
+    @When("usuario acessa o produto escolhido na pagina Products")
+    public void usuarioAcessaOProdutoEscolhidoNaPaginaProducts() {
+        page.accessProductByName(Hooks.productDto.getName());
+    }
+
     @Then("usuario e direcionado a pagina Products")
     public void usuarioEDirecionadoAPaginaProducts() {
         Assert.assertEquals("Products", page.getTitle());
+    }
+
+    @Then("os detalhes do produto persistem ao serem apresentados individualmente")
+    public void osDetalhesDoProdutoPersistemAoSeremApresentadosIndividualmente() {
+        int quantityOfProducts = page.getQuantityOfProducts();
+        Assert.assertEquals(1, quantityOfProducts);
+        Assert.assertEquals(Hooks.productDto, page.getProductByIndex(0));
     }
 
 }
