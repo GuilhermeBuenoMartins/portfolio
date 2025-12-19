1b009a0a420a020913f14f65f87a32a6ae9b70ad
Guilherme Bueno Martins <bmartins.guilherme.2022@gmail.com>
2025-12-19 19:52:57 -0300

    feat: Response for user signed up successfully

--- visit-me-api/src/main/java/org/example/visitme/view/controllers/UserController.java
+++ visit-me-api/src/main/java/org/example/visitme/view/controllers/UserController.java
@@ -1,11 +1,16 @@
 package org.example.visitme.view.controllers;
 
+import java.time.Instant;
+
 import org.example.visitme.control.dto.UserDto;
 import org.example.visitme.control.services.UserService;
 import org.example.visitme.control.services.ValidationService;
 import org.example.visitme.utils.ConverterUtil;
 import org.example.visitme.view.requests.UserRequest;
+import org.example.visitme.view.responses.Response;
+import org.example.visitme.view.responses.UserResponse;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -26,13 +31,16 @@ public class UserController {
     private ValidationService validation;
 
     @PostMapping("/sign-up")
-    public ResponseEntity<Object> signUp(@RequestBody UserRequest request) {
+    public ResponseEntity<Response<UserResponse>> signUp(@RequestBody UserRequest request) {
+        final String MESSAGE = "You were signed up successfully.";
         validation.signUp(request);
         BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
         request.getLogin().setPassword(encoder.encode((String) request.getLogin().getPassword()));
         UserDto dto = (UserDto) ConverterUtil.from(request, UserDto.class);
         dto = service.insert(dto);
-        return new ResponseEntity<>(HttpStatusCode.valueOf(201));
+        UserResponse userResponse = ConverterUtil.from(dto, UserResponse.class);
+        Response<UserResponse> response = new Response<>(Instant.now(), HttpStatus.CREATED, MESSAGE, userResponse);
+        return new ResponseEntity<>(response, HttpStatusCode.valueOf(201));
     }
     
 }
